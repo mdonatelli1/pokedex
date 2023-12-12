8313db6fe057f439b9287a60009c74c695e78349
Mattéo Donatelli <matteo.donatelli1@gmail.com>
2023-12-12 10:59:16 +0100

    Type props with prop-types

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,5 +1,13 @@
-function PokemonCard({pokemon}) {
+import PropTypes from "prop-types";
 
+PokemonCard.propTypes = {
+    pokemon: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        imgSrc: PropTypes.string
+    }).isRequired
+}
+
+function PokemonCard({pokemon}) {
     return (
         <figure>
             {pokemon.imgSrc !== undefined ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
